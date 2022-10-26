197dd3fd83ea8e6549ddec5f1fd6f5424b745c70
Richard Poroszlay <richardporoszlay@gmail.com>
2022-10-26 15:10:50 +0200

    Upload 12 and 13

---
+++ Exercises/feladat12.cpp
@@ -0,0 +1,30 @@
+/*
+	1. Olvassa be a gombokat tartalmazó képet.
+	2. Küszöbölje a képet(a fehér gombok elveszhetnek)
+	3. Dilatálja a képet 7x7 - es, ellipszis alakú struktúraelemmel.
+	4. Jelenítse meg a kapott eredményt.
+*/
+
+#include <iostream>
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgproc.hpp>
+
+using namespace std;
+using namespace cv;
+
+
+int main() {
+
+	Mat gombok = imread("Kepek/color_buttons.jpg", IMREAD_COLOR);
+	Mat gray, binary;
+	cvtColor(gombok, gray, COLOR_BGR2GRAY);
+	threshold(gray, binary, 160, 255, THRESH_BINARY_INV);
+	Mat S = getStructuringElement(MORPH_ELLIPSE, Size(7, 7));
+	Mat dil;
+	dilate(binary, dil, S);
+	imshow("dil", dil);
+	waitKey(0);
+
+	return 0;
+}
